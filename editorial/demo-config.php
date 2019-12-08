8b2675f781b5c7ee8e7140065c1807706fabef01
Raaz Shrestha <15938292+shrestharaaz@users.noreply.github.com>
2019-12-08 16:15:25 +0545

    update all required files and demo config and json files

--- editorial/demo-config.php
+++ editorial/demo-config.php
@@ -19,17 +19,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 function mt_demo_importer_config(){
 	
 	$theme_slug 	= 'editorial';
-	$git_url 		= 'https://raw.githubusercontent.com/mysterythemes/themes-demo-pack/master/'.$theme_slug.'/';
+	$git_url 		= 'https://raw.githubusercontent.com/mysterythemes/themes-demo-pack/master/'.$theme_slug.'/default/';
 	$demo_config 	= array(
 		'default' => array(
 			'demo_name'			=> esc_html__( 'Default Demo', 'mystery-demo-importer' ),
 			'theme_slug'        => $theme_slug,
 			'theme_description' => esc_html__( 'Ultimate responsive magazine WordPress Theme.', 'mystery-demo-importer' ),
-			'xml_file'     		=> $git_url . 'default/'.$theme_slug.'.xml',
-			'theme_settings' 	=> $git_url . 'default/'.$theme_slug.'-export.dat',
-			'widgets_file'  	=> $git_url . 'default/'.$theme_slug.'-widgets.wie',
-			'preview_screen'	=> $git_url . 'default/screenshot.jpg',
-			'preview_url'		=> 'http://demo.mysterythemes.com/'.$theme_slug.'/',
+			'xml_file'     		=> $git_url . $theme_slug.'.xml',
+			'theme_settings' 	=> $git_url . $theme_slug.'-export.dat',
+			'widgets_file'  	=> $git_url . $theme_slug.'-widgets.wie',
+			'preview_screen'	=> $git_url . 'screenshot.jpg',
+			'preview_url'		=> 'https://demo.mysterythemes.com/'.$theme_slug.'/',
 			'is_shop'			=> false,
 			'theme_type'        => array( 'free', 'blog', 'magazine' ),
 			'home_title'  		=> 'Home',
