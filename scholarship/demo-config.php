3e6bc9cea05f9abdb14544802de5283fa30f41f4
Raaz Shrestha <15938292+shrestharaaz@users.noreply.github.com>
2019-12-09 13:16:06 +0545

    modified json file along with changing extension of screenshot

--- scholarship/demo-config.php
+++ scholarship/demo-config.php
@@ -19,20 +19,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 function mt_demo_importer_config(){
 	
 	$theme_slug 	= 'scholarship';
-	$git_url 		= 'https://raw.githubusercontent.com/mysterythemes/themes-demo-pack/master/'.$theme_slug.'/';
+	$git_url 		= 'https://raw.githubusercontent.com/mysterythemes/themes-demo-pack/master/'.$theme_slug.'/default/';
 	$demo_config 	= array(
 		'default' => array(
 			'demo_name'			=> esc_html__( 'Default Demo', 'mystery-demo-importer' ),
 			'theme_slug'        => $theme_slug,
 			'theme_description' => esc_html__( 'Best Free WordPress Education Theme.', 'mystery-demo-importer' ),
-			'xml_file'     		=> $git_url . 'default/'.$theme_slug.'.xml',
-			'theme_settings' 	=> $git_url . 'default/'.$theme_slug.'-export.dat',
-			'widgets_file'  	=> $git_url . 'default/'.$theme_slug.'-widgets.wie',
-			'preview_screen'	=> $git_url . 'default/screenshot.jpg',
+			'xml_file'     		=> $git_url . $theme_slug.'.xml',
+			'theme_settings' 	=> $git_url . $theme_slug.'-export.dat',
+			'widgets_file'  	=> $git_url . $theme_slug.'-widgets.wie',
+			'preview_screen'	=> $git_url . 'screenshot.png',
 			'preview_url'		=> 'https://demo.mysterythemes.com/'.$theme_slug.'/',
 			'is_shop'			=> false,
+			'theme_type'        => array( 'free', 'education', 'portfolio' ),
 			'home_title'  		=> 'Home',
-			'blog_title'  		=> '',
+			'blog_title'  		=> 'Blog',
 			'menus_locations'	=> array(
 									'scholarship_primary_menu' => 'Primary Menu',
 								),
