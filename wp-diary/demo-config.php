aef85535680a09693686428aa91a4ffeb417d1c4
Raaz Shrestha <15938292+shrestharaaz@users.noreply.github.com>
2019-12-09 13:19:54 +0545

    modified json file along with changing extension of screenshot at wp-diary

--- wp-diary/demo-config.php
+++ wp-diary/demo-config.php
@@ -19,16 +19,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 function mt_demo_importer_config(){
 
 	$theme_slug 	= 'wp-diary';
-	$git_url 		= 'https://raw.githubusercontent.com/mysterythemes/themes-demo-pack/master/'.$theme_slug.'/';
+	$git_url 		= 'https://raw.githubusercontent.com/mysterythemes/themes-demo-pack/master/'.$theme_slug.'/default/';
 	$demo_config 	= array(
 		'default' => array(
 			'demo_name'			=> esc_html__( 'Default Demo', 'mystery-demo-importer' ),
 			'theme_slug'        => $theme_slug,
 			'theme_description' => esc_html__( 'A pure blogger free WordPress Theme.', 'mystery-demo-importer' ),
-			'xml_file'     		=> $git_url . 'wp-diary-default/'.$theme_slug.'.xml',
-			'theme_settings' 	=> $git_url . 'wp-diary-default/'.$theme_slug.'-export.dat',
-			'widgets_file'  	=> $git_url . 'wp-diary-default/'.$theme_slug.'-widgets.wie',
-			'preview_screen'	=> $git_url . 'wp-diary-default/screenshot.jpg',
+			'xml_file'     		=> $git_url . $theme_slug.'.xml',
+			'theme_settings' 	=> $git_url . $theme_slug.'-export.dat',
+			'widgets_file'  	=> $git_url . $theme_slug.'-widgets.wie',
+			'preview_screen'	=> $git_url . 'screenshot.png',
 			'preview_url'		=> 'https://demo.mysterythemes.com/'.$theme_slug.'/',
 			'is_shop'			=> false,
 			'theme_type'        => array( 'free', 'blog', 'portfolio' ),
