fcbc2fc8daf7b0ab20a0d6294e1b6609c21c3536
Raaz Shrestha <15938292+shrestharaaz@users.noreply.github.com>
2020-01-05 22:06:50 +0545

    update the config and json file.

--- fotogenic/demo-config.php
+++ fotogenic/demo-config.php
@@ -20,31 +20,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 function mt_demo_importer_config(){
 
     $theme_slug 	= 'fotogenic';
-	$git_url 		= 'https://raw.githubusercontent.com/mysterythemes/themes-demo-pack/master/'.$theme_slug.'/default/';
+	$git_url 		= 'https://raw.githubusercontent.com/mysterythemes/themes-demo-pack/master/'.$theme_slug.'/';
 	$demo_config 	= array(
-		'default' => array(
-			'demo_name'			=> esc_html__( 'Default Demo', 'mystery-demo-importer' ),
+		'fotogenic' => array(
+			'name'			=> esc_html__( 'Fotogenic', 'mystery-demo-importer' ),
 			'theme_slug'        => $theme_slug,
+			'version'			=> '1.0.7',
 			'theme_description' => esc_html__( 'A clean and powerful photography WordPress Theme.', 'mystery-demo-importer' ),
-			'xml_file'     		=> $git_url . $theme_slug.'.xml',
-			'theme_settings' 	=> $git_url . $theme_slug.'-export.dat',
-			'widgets_file'  	=> $git_url . $theme_slug.'-widgets.wie',
-			'preview_screen'	=> $git_url . 'screenshot.png',
+			'xml_file'     		=> $git_url . 'default/'.$theme_slug.'.xml',
+			'theme_settings' 	=> $git_url . 'default/'.$theme_slug.'-export.dat',
+			'widgets_file'  	=> $git_url . 'default/'.$theme_slug.'-widgets.wie',
+			'preview_screen'	=> $git_url . 'default/screenshot.png',
 			'preview_url'		=> 'https://demo.mysterythemes.com/'.$theme_slug.'/',
 			'is_shop'			=> false,
 			'theme_type'        => array( 'free', 'photography', 'portfolio' ),
+			'show_on_front'     => '',
 			'home_title'  		=> '',
 			'blog_title'  		=> '',
 			'menus_locations'	=> array(
 			   'menu-1' => 'main menu',
 			),
-			'required_plugins'  => array(
-				'free-plugins' => array(
-					array(
-						'plugin_name'   => __( 'Contact Form 7', 'mystery-demo-importer' ),
-						'plugin_slug'	=> 'contact-form-7',
-						'install_setup' => 'contact-form-7/wp-contact-form-7.php',
-					)
+			'plugins_list' => array(
+				'contact-form-7' => array(
+					'name'		=> __( 'Contact Form 7', 'mystery-demo-importer' ),
+					'slug'		=> 'contact-form-7/wp-contact-form-7.php',
+					'required'	=> true
 				)
 			)
 		)
