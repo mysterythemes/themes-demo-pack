4aaeb86681c69290714ac18546de2d1994a09f2e
Raaz Shrestha <15938292+shrestharaaz@users.noreply.github.com>
2020-01-05 22:19:34 +0545

    update config and json file of demo

--- owner/demo-config.php
+++ owner/demo-config.php
@@ -19,66 +19,68 @@ if ( ! defined( 'ABSPATH' ) ) {
 function mt_demo_importer_config(){
 
 	$theme_slug 	= 'owner';
-	$git_url 		= 'https://raw.githubusercontent.com/mysterythemes/themes-demo-pack/master/'.$theme_slug.'/default';
+	$pro_theme_slug = 'owner-pro';
+	$git_url 		= 'https://raw.githubusercontent.com/mysterythemes/themes-demo-pack/master/'.$theme_slug.'/';
 	$demo_config 	= array(
 		'owner' => array(
-			'demo_name'			=> esc_html__( 'Owner', 'mystery-demo-importer' ),
+			'name'				=> esc_html__( 'Owner', 'mystery-demo-importer' ),
 			'theme_slug'        => $theme_slug,
+			'version'			=> '1.1.8',
 			'theme_description' => esc_html__( 'A clean multipurpose WordPress Theme.', 'mystery-demo-importer' ),
-			'xml_file'     		=> $git_url . $theme_slug.'.xml',
-			'theme_settings' 	=> $git_url . $theme_slug.'-export.dat',
-			'widgets_file'  	=> $git_url . $theme_slug.'-widgets.wie',
-			'preview_screen'	=> $git_url . 'screenshot.png',
+			'xml_file'     		=> $git_url . 'default/'.$theme_slug.'.xml',
+			'theme_settings' 	=> $git_url . 'default/'.$theme_slug.'-export.dat',
+			'widgets_file'  	=> $git_url . 'default/'.$theme_slug.'-widgets.wie',
+			'preview_screen'	=> $git_url . 'default/screenshot.png',
 			'preview_url'		=> 'https://demo.mysterythemes.com/'.$theme_slug.'/',
 			'is_shop'			=> false,
 			'theme_type'        => array( 'free', 'business', 'multipurpose' ),
+			'show_on_front'     => 'page',
 			'home_title'  		=> 'Home',
 			'blog_title'  		=> 'Blogs',
 			'menus_locations'	=> array(
-									'owner_primary_menu' 	=> 'Primary Menu',
-									'owner_footer_menu'		=> 'Footer Menu'
-								),
-			'required_plugins'  => array(
-				'free' => array(
-					array(
-						'plugin_name'		=> esc_html__( 'Contact Form 7', 'mystery-demo-importer' ),
-						'plugin_slug'  		=> 'contact-form-7',
-						'install_setup'  	=> 'contact-form-7/wp-contact-form-7.php'
-					)
+				'owner_primary_menu' 	=> 'Primary Menu',
+				'owner_footer_menu'		=> 'Footer Menu'
+			),
+			'plugins_list' => array(
+				'contact-form-7' => array(
+					'name'=> __( 'Contact Form 7', 'mystery-demo-importer' ),
+					'slug'=> 'contact-form-7/wp-contact-form-7.php',
+					'required'=> true
 				)
 			)
 		),
 
 		'owner-pro' => array(
-			'demo_name'			=> esc_html__( 'Owner Pro', 'mystery-demo-importer' ),
-			'theme_slug'        => $theme_slug,
+			'name'				=> esc_html__( 'Owner Pro', 'mystery-demo-importer' ),
+			'theme_slug'        => $pro_theme_slug,
+			'version'			=> '1.5.2',
 			'theme_description' => esc_html__( 'A clean multipurpose premium WordPress Theme.', 'mystery-demo-importer' ),
-			'xml_file'     		=> $git_url . 'owner-pro/'.$theme_slug.'.xml',
-			'theme_settings' 	=> $git_url . 'owner-pro/'.$theme_slug.'-export.dat',
-			'widgets_file'  	=> $git_url . 'owner-pro/'.$theme_slug.'-widgets.wie',
-			'preview_screen'	=> $git_url . 'owner-pro/screenshot.png',
-			'preview_url'		=> 'https://demo.mysterythemes.com/'.$theme_slug.'-pro/',
+			'xml_file'     		=> $git_url . 'owner-pro-default/'.$pro_theme_slug.'.xml',
+			'theme_settings' 	=> $git_url . 'owner-pro-default/'.$pro_theme_slug.'-export.dat',
+			'widgets_file'  	=> $git_url . 'owner-pro-default/'.$pro_theme_slug.'-widgets.wie',
+			'preview_screen'	=> $git_url . 'owner-pro-default/screenshot.png',
+			'preview_url'		=> 'https://demo.mysterythemes.com/'.$pro_theme_slug,
 			'is_shop'			=> true,
 			'theme_type'        => array( 'premium', 'business', 'multipurpose' ),
+			'show_on_front'     => 'page',
 			'home_title'  		=> 'Home',
 			'blog_title'  		=> 'Blogs',
 			'menus_locations'	=> array(
-									'owner_primary_menu' 	=> 'Primary Menu',
-									'owner_footer_menu'		=> 'Footer Menu'
-								),
-			'required_plugins'  => array(
-				'free' => array(
-					array(
-						'plugin_name'		=> esc_html__( 'Contact Form 7', 'mystery-demo-importer' ),
-						'plugin_slug'  		=> 'contact-form-7',
-						'install_setup'  	=> 'contact-form-7/wp-contact-form-7.php'
-					),
-					array(
-						'plugin_name' 	=> __( 'WooCommerce', 'mystery-demo-importer' ),
-						'plugin_slug'	=> 'woocommerce',
-						'install_setup' => 'woocommerce/woocommerce.php',
-					)
-				)
+				'owner_primary_menu' 	=> 'Primary Menu',
+				'owner_footer_menu'		=> 'Footer Menu'
+			),
+			'plugins_list' => array(
+				'contact-form-7' => array(
+					'name'		=> __( 'Contact Form 7', 'mystery-demo-importer' ),
+					'slug'		=> 'contact-form-7/wp-contact-form-7.php',
+					'required'	=> true
+				),
+				'woocommerce' => array(
+					'name'		=> __( 'WooCommerce', 'mystery-demo-importer' ),
+					'slug'		=> 'woocommerce/woocommerce.php',
+					'required'	=> true
+				),
+				
 			)
 		)
 	);
