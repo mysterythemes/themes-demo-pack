c65a4e6b369b0a53168396753699f09b41a07dad
Raaz Shrestha <15938292+shrestharaaz@users.noreply.github.com>
2020-01-06 13:07:53 +0545

    update demo config and json file.

--- swipewp/demo-config.php
+++ swipewp/demo-config.php
@@ -20,36 +20,36 @@ if ( ! defined( 'ABSPATH' ) ) {
 function mt_demo_importer_config(){
 
     $theme_slug     = 'swipewp';
-    $git_url        = 'https://raw.githubusercontent.com/mysterythemes/themes-demo-pack/master/'.$theme_slug.'/default/';
+    $git_url        = 'https://raw.githubusercontent.com/mysterythemes/themes-demo-pack/master/'.$theme_slug.'/';
     $demo_config    = array(
-        'default' => array(
-            'demo_name'         => esc_html__( 'Default Demo', 'mystery-demo-importer' ),
+        'swipewp' => array(
+            'name'              => esc_html__( 'Swipewp', 'mystery-demo-importer' ),
             'theme_slug'        => $theme_slug,
+            'version'           => '1.0.5',
             'theme_description' => esc_html__( 'Powerful Multipurpose Free WordPress Theme.', 'mystery-demo-importer' ),
-            'xml_file'          => $git_url . $theme_slug.'.xml',
-            'theme_settings'    => $git_url . $theme_slug.'-export.dat',
-            'widgets_file'      => $git_url . $theme_slug.'-widgets.wie',
-            'preview_screen'    => $git_url . 'screenshot.png',
+            'xml_file'          => $git_url . 'default/'.$theme_slug.'.xml',
+            'theme_settings'    => $git_url . 'default/'.$theme_slug.'-export.dat',
+            'widgets_file'      => $git_url . 'default/'.$theme_slug.'-widgets.wie',
+            'preview_screen'    => $git_url . 'default/screenshot.png',
             'preview_url'       => 'https://demo.mysterythemes.com/'.$theme_slug.'/',
             'is_shop'           => true,
             'theme_type'        => array( 'free', 'multipurpose', 'woocommerce', 'portfolio' ),
+            'show_on_front'     => 'page',
             'home_title'        => 'Home',
             'blog_title'        => '',
             'menus_locations'   => array(
                'primary_menu' => 'Header Menu',
             ),
-            'required_plugins'  => array(
-                'free-plugins' => array(
-                    array(
-                        'plugin_name'   => __( 'Contact Form 7', 'mystery-demo-importer' ),
-                        'plugin_slug'   => 'contact-form-7',
-                        'install_setup' => 'contact-form-7/wp-contact-form-7.php',
-                    ),
-                    array(
-                        'plugin_name'   => __( 'WooCommerce', 'mystery-demo-importer' ),
-                        'plugin_slug'   => 'woocommerce',
-                        'install_setup' => 'woocommerce/woocommerce.php',
-                    )
+            'plugins_list' => array(
+                'contact-form-7' => array(
+                    'name'      => __( 'Contact Form 7', 'mystery-demo-importer' ),
+                    'slug'      => 'contact-form-7/wp-contact-form-7.php',
+                    'required'  => true
+                ),
+                'woocommerce' => array(
+                    'name'      => __( 'WooCommerce', 'mystery-demo-importer' ),
+                    'slug'      => 'woocommerce/woocommerce.php',
+                    'required'  => true
                 )
             )
         )
