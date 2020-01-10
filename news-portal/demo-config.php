8eba94c776ee6e5664a5c03d17269d9eac0f1cc6
Raaz Shrestha <15938292+shrestharaaz@users.noreply.github.com>
2020-01-10 10:00:47 +0545

    changes the folder name and update on demo config and json file.

--- news-portal/demo-config.php
+++ news-portal/demo-config.php
@@ -22,14 +22,14 @@ function mt_demo_importer_config(){
 	$git_url 		= 'https://raw.githubusercontent.com/mysterythemes/themes-demo-pack/master/'.$theme_slug.'/';
 	$demo_config 	= array(
 		'news-portal' => array(
-			'name'			=> esc_html__( 'News Portal', 'mystery-demo-importer' ),
+			'name'				=> esc_html__( 'News Portal', 'mystery-demo-importer' ),
 			'theme_slug'        => $theme_slug,
-			'version' => '1.1.19',
+			'version' 			=> '1.1.19',
 			'theme_description' => esc_html__( 'Ultimate responsive magazine WordPress Theme.', 'mystery-demo-importer' ),
-			'xml_file'     		=> $git_url . 'default/'.$theme_slug.'.xml',
-			'theme_settings' 	=> $git_url . 'default/'.$theme_slug.'-export.dat',
-			'widgets_file'  	=> $git_url . 'default/'.$theme_slug.'-widgets.wie',
-			'preview_screen'	=> $git_url . 'default/screenshot.png',
+			'xml_file'     		=> $git_url . 'news-portal/'.$theme_slug.'.xml',
+			'theme_settings' 	=> $git_url . 'news-portal/'.$theme_slug.'-export.dat',
+			'widgets_file'  	=> $git_url . 'news-portal/'.$theme_slug.'-widgets.wie',
+			'preview_screen'	=> $git_url . 'news-portal/screenshot.png',
 			'preview_url'		=> 'http://demo.mysterythemes.com/'.$theme_slug.'/',
 			'is_shop'			=> false,
 			'theme_type'        => array( 'free', 'blog', 'magazine' ),
@@ -86,9 +86,9 @@ function mt_demo_importer_config(){
 			),
 			'plugins_list' => array(
 				'contact-form-7' => array(
-					'name'=> 'Contact Form 7',
-					'slug'=> 'contact-form-7/wp-contact-form-7.php',
-					'required'=> true
+					'name'		=> 'Contact Form 7',
+					'slug'		=> 'contact-form-7/wp-contact-form-7.php',
+					'required'	=> true
 				)
 			 )
 		)
